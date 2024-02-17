8152698bebe502b994a597c534ebb50372467337
Josh Ashton <joshua.t.ashton@gmail.com>
2024-02-16 17:01:43 -0700

    wifi disconnect/connect functionality implemented. need to fix the updating ui and formatting.

--- wifi.py
+++ wifi.py
@@ -2,6 +2,7 @@ import customtkinter as ctk
 import os
 
 from button import Button
+from functools import partial
 
 # default colors
 base = '#262940'
@@ -9,6 +10,7 @@ accent = '#4D5382'
 hover = '#C6CAED'
 
 class Wifi(ctk.CTk):
+    networks = []
     def __init__(self, callback, base, accent, hover):
         super().__init__()
         base = base
@@ -22,16 +24,15 @@ class Wifi(ctk.CTk):
         self.set_size()
         self.resizable(False, False)
 
-        networks = self.read_networks()
+        self.read_networks()
         
         self.create_layout(5)
-        self.create_widgets(networks, 5)
+        self.create_widgets(5)
 
         self.mainloop()
 
     def read_networks(self):
         os.system('net-info -l')
-        networks = []
 
         # Print each line in the file
         with open('/tmp/networks.txt', 'r') as f:
@@ -41,9 +42,7 @@ class Wifi(ctk.CTk):
                 if line == '\n':
                     continue
 
-                networks.append(line[:-1])
-
-        return networks
+                self.networks.append(line[:-1])
         
     # Overlay on top of the Tray.
     def set_size(self):
@@ -66,20 +65,45 @@ class Wifi(ctk.CTk):
         self.columnconfigure(0, weight=1)
 
     # Create the display and button widgets
-    def create_widgets(self, networks, n):
+    def create_widgets(self, n):
         for i in range(n):
             # If this is the first network and it has , then it is active
-            if i == 0 and networks[i].startswith(''):
-                b = Button(self, networks[i][:-1] + ' ', 'active', self.connect, i, 0, 2, 'nsew')
+            if i == 0 and self.networks[i].startswith(''):
+                b = Button(self, self.networks[i][:-1] + ' ', 'active', partial(self.connect, i, 1), i, 0, 2, 'nsew')
 
             else:
-                b = Button(self, networks[i] + ' ', 'inactive', self.connect, i, 0, 2, 'nsew')
+                b = Button(self, self.networks[i] + ' ', 'inactive', partial(self.connect, i, 2), i, 0, 2, 'nsew')
 
             b.grid(row=i, column=0, ipadx=2, ipady=5, columnspan=1, sticky='nsew')
             b.set_font(('Lato', 18))
             b.configure(anchor='w')
 
-    def connect(self):
-        print('connect')
-        self.withdraw()
-        self.callback()
+    def connect(self, i, active):
+
+        if active == 1:
+            os.system('iwctl station wlan0 disconnect')
+            print('Disconnecting from ', self.networks[i])
+            self.withdraw()
+            self.callback()
+            return
+
+        # Print each line in the file
+        with open('/tmp/available.network', 'r') as f:
+            curr = 0
+
+            for line in f:
+
+                # If the line is empty, skip it
+                if line == '\n':
+                    continue
+
+                if curr == i:
+                    os.system('iwctl station wlan0 connect ' + line[:-1])
+                    print('Connecting to ', self.networks[i])
+                    self.withdraw()
+                    self.callback()
+                    return
+                
+                curr += 1
+                
+        print('No network found')
